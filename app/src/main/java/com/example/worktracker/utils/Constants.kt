4affbe163d5bb05e799148dae7930c4ea1de2752
Cristóbal Sepúlveda Silva <sepulveda.cristobal.ignacio@gmail.com>
2023-07-09 20:01:33 -0400

    authentication activity funciona como es esperado. falta hacer el logout y esta listo

--- app/src/main/java/com/example/worktracker/utils/Constants.kt
+++ app/src/main/java/com/example/worktracker/utils/Constants.kt
@@ -1,8 +1,11 @@
 package com.example.worktracker.utils
 
+import com.google.firebase.auth.FirebaseAuth
+
 object Constants{
     const val PACKAGE_NAME = "com.example.soluemergencias"
     internal const val ACTION_LOCATION_BROADCAST = "$PACKAGE_NAME.action.LOCATION_BROADCAST"
     internal const val EXTRA_LOCATION = "$PACKAGE_NAME.extra.LOCATION"
     const val NOTIFICATION_CHANNEL_ID = "localizacion"
+    val firebaseAuth = FirebaseAuth.getInstance()
 }
